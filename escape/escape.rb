a16858e83dc86c832f65517526dccd1b4fb465f2
Peter Cooper <git@peterc.org>
2011-08-23 02:04:12 +0100

    Added loaders for new classes

--- escape/escape.rb
+++ escape/escape.rb
@@ -6,9 +6,12 @@ require 'java'
 require 'input_handler'
 require 'component'
 require 'game'
+require 'sound'
 
 require 'gui/bitmap'
 require 'gui/screen'
+require 'gui/sprite'
+require 'gui/rubble_sprite'
 
 require 'menu/menu'
 require 'menu/title_menu'
