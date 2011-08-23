7355b42bf717ac9c8f0aa9c88020de2b150b860a
Peter Cooper <git@peterc.org>
2011-08-23 02:01:09 +0100

    Added Sprite class

---
+++ escape/gui/sprite.rb
@@ -0,0 +1,14 @@
+class Sprite
+  attr_accessor :removed, :x, :y, :z, :tex, :col
+  
+  def initialize(x, y, z, tex, color)
+    @x = x
+    @y = y
+    @z = z
+    @tex = text
+    @col = color || 0x202020
+    @removed = false
+  end
+  
+  def tick; end
+end
