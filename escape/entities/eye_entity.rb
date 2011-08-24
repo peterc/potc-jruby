cb0fe40883a4250cb4ecb51fabcd853a30b38a25
Peter Cooper <git@peterc.org>
2011-08-24 18:29:48 +0100

    Fixed a paren issue

--- escape/entities/eye_entity.rb
+++ escape/entities/eye_entity.rb
@@ -1,6 +1,6 @@
 class EyeEntity < EnemyEntity
   def initialize(x, z)
-    super(x, z, 4 * 8 + 4, Art.get_col(0x84ECFF)
+    super(x, z, 4 * 8 + 4, Art.get_col(0x84ECFF))
     @health = 4
     @r = 0.3
     @run_speed = 2
